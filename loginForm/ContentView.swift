19ab54e9c9ef789890613f71bf9e9fdbc15b23b9
Theoderic onipe <theodericonipe@Theoderics-MacBook-Pro-2.local>
2023-09-05 16:18:58 +0200

    login form swift ui

---
+++ loginForm/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  loginForm
+//
+//  Created by Theoderic onipe on 9/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack{
+                Color.blue.ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.3)
+                    .foregroundColor(.white)
+                VStack{
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width:300, height:50)
+                        .background(Color.black.opacity(0.05))
+                        .border(.red, width: CGFloat(wrongUsername))
+                        .cornerRadius(10)
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    Button("Login"){
+                        authenticateUser(username: username, password: password)
+                    }
+                    .frame(width:300, height: 50)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen){
+                        EmptyView()
+                    }
+                }
+            }.navigationBarHidden(true)
+        }
+    }
+    func authenticateUser(username: String, password: String){
+        if username.lowercased() == "mario" {
+            wrongUsername = 0
+            if password.lowercased() == "abc123" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        }
+        else {
+            wrongUsername = 2
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ loginForm/loginFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginFormApp.swift
+//  loginForm
+//
+//  Created by Theoderic onipe on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct loginFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
